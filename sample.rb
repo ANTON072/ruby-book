6b6ae2c938986e7464d3bc87fec7dfdeb30d2f37
Katsushi OUGI <bdbkx286@gmail.com>
2019-01-02 13:05:35 +0900

    P115

--- sample.rb
+++ sample.rb
@@ -1,7 +1,17 @@
-require "minitest/autorun"
-
-class SampleTest < Minitest::Test
-  def test_sample
-    assert_equal "RUBY", 'ruby'.capitalize
+def charge(age)
+  case age
+  when 0..5
+    0
+  when 6..15
+    300
+  when 13..18
+    600
+  else
+    1000
   end
 end
+
+puts charge(3)
+puts charge(12)
+puts charge(16)
+puts charge(25)
